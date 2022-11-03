3c2ad8f82dabb3171e1b3f91cb28e2ff35779096
saarger <saargerassi@mail.tau.ac.il>
2022-11-03 14:00:39 +0200

    tttt

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import numpy as np
 import scipy
 from scipy.stats import binom
 import matplotlib.pyplot as plt
-import seaborn as sns
+
 
 
 #Q2.a
@@ -46,24 +46,16 @@ def make_cdf():
 #Q2.d
 def ploting():
     m = make_cdf()
+    print(m)
     x = m[:,0]
     y = m[:,1]
-    print("x is:" , x)
-    print("y is:" , y)
-    plt.scatter(x,y)
+    plt.ylim(0,1)
+    plt.step(x,y)
     plt.show()
 
-ploting()
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    X = binom.rvs(n = 5, p  = 1/6,size =  20)
-    # print(X)
-    # m = Empirical_F(X)
-    # y = m[:,1]
-    # y = np.transpose(y)
-    # print(y)
-    # print(X)
-    # plt.ylim(0,1)
-    # plt.step(X,y)
-    # plt.show()
+    ploting()
+
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
